6282e7a41338cfdbfc5a7ed324c9b7e03a680d1b
Sam Lijo <samuelvillalvalijo@gmail.com>
2021-02-17 20:58:53 -0600

    Error Segmentation fault: 11

---
+++ first_file.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int five = 5;
+    int six = 6;
+    int i;
+
+    int numbers[12];
+
+    int sum = five + six;
+
+    cout << "five + six = "<<sum<<endl;
+
+    cout<<"MY NAME IS SAM"<<endl;
+
+    cout<<"Enter some Ints into an array";
+
+
+    // for (i=0; i<12; i++){
+    //     cout<<"Enter an Int into the array: ";
+    //     cin >> numbers[i];
+    // }
+    // cout<< "You entered:"<<endl;
+
+    // for(i=0; i<12; i++){
+    //     cout<<numbers[i];
+
+    // }
+
+    // Now to create an array of Names
+
+    char Names[5][20];
+
+    //use a for loop to iterate through the array and load it with names
+
+    cout<<"Enter some names into this Charecter array:"<<endl;
+
+    for(int j=0;j<5;j++){
+        cout<<"Enter in a Name: "; cin>>Names[i];
+    }
+
+    cout<<"Now we will read all the names you entered back too you!";
+
+    for(int j=0;j<5;j++){
+        cout<<Names[j]<<endl;
+    }
+
+}
